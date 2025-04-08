e8bc700010df1289bae1a2ea2b68efacc0bc9153
ManojBaasha <manoj.elango123@gmail.com>
2025-04-08 14:55:05 -0700

    working animations

---
+++ swift-radar-snippet/swift-radar-snippet/swift_radar_snippetApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_radar_snippetApp.swift
+//  swift-radar-snippet
+//
+//  Created by Manoj Elango on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct swift_radar_snippetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift-radar-snippet/swift-radar-snippetTests/swift_radar_snippetTests.swift
@@ -0,0 +1,17 @@
+//
+//  swift_radar_snippetTests.swift
+//  swift-radar-snippetTests
+//
+//  Created by Manoj Elango on 4/8/25.
+//
+
+import Testing
+@testable import swift_radar_snippet
+
+struct swift_radar_snippetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
